bd72e3671f991982908199543b04c7c34cbc6a78
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-04-23 14:39:20 -0400

    added html,css,js

---
+++ assets/chri.js
@@ -0,0 +1,8 @@
+// Get the h2 element with the text "Menu"
+var menuElement = document.getElementById("menu");
+
+// Add an event listener to the menu element
+menuElement.addEventListener("click", function() {
+  // Navigate to the "menu.html" page
+  window.location.href = "chri.html";
+});
